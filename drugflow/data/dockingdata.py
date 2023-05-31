649788fcb6a913d525eda7b1ba38d9f38f5ed698
daiyizheng <387942239@qq.com>
2023-05-31 16:43:26 +0800

    add  pdbbind

--- drugflow/data/dockingdata.py
+++ drugflow/data/dockingdata.py
@@ -10,29 +10,51 @@ Version          :1.0
 
 from __future__ import absolute_import, print_function, annotations
 import logging, os
+from typing import Text, Dict
 
+import dataclasses
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
+@dataclasses.dataclass
+class DockingExamples:
+    name:Text
+    ligand_path: Text
+    receptor_path: Text
+    autobox_ligand: Text = None
+    process_ligand_path:Text = None
+    process_receptor_path:Text = None
+    config_path: Text = None
+    output_path:Text = None
+    middleware:Dict = dataclasses.field(default_factory=dict)
+    metirics:Dict = dataclasses.field(default_factory=dict)
+    success:int = 0
 
 class DockingData(object):
     def __init__(self) -> None:
         self.data = []
         self.index = -1
         
-    def load_data(self, path):
+    def load_data(self, 
+                  path:Text, 
+                  prefix={"ligand":"ligand", "receptor":"protein_processed", "config":"config.txt"}, 
+                  format={"ligand":"sdf", "receptor":"pdb"}):
+                      
         file_name_list = os.listdir(path)
         for name in tqdm(file_name_list):
-            ligand_path = os.path.join(path, name, name + "_ligand.sdf")
-            receptor_path = os.path.join(path, name,  name + "_protein_processed.pdb")
-            config_path = os.path.join(path, name, "config.txt")
+            ligand_path = os.path.join(path, name, f"{name}_{prefix['ligand']}.{format['ligand']}")
+            receptor_path = os.path.join(path, name,  f"{name}_{prefix['receptor']}.{format['receptor']}")
+            config_path = os.path.join(path, name, prefix['config'])
             
             if not os.path.exists(config_path):
-                config_path = None
+                config_path = None  
+
+            self.data.append(DockingExamples(name=name,
+                                             ligand_path=ligand_path, 
+                                             receptor_path=receptor_path, 
+                                             config_path=config_path))
             
-            self.data.append((ligand_path, receptor_path, config_path, None))
-    
     def __len__(self):
         return len(self.data)
     
